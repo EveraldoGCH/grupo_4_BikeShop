5d309aba2729a250f13a5c7654ad53f4e43a7b8a
EveraldoGCH <everaldoguevara@gmail.com>
2021-05-08 19:14:40 -0300

    agregado multer y cosas varias

--- TiendaFunkoShop/router/mainRouter.js
+++ TiendaFunkoShop/router/mainRouter.js
@@ -1,7 +1,19 @@
 const mainController = require('../controllers/mainController');
 const controllerProductos = require('../controllers/controllerProductos');
 const express=require('express')
+const multer=require('multer')
+const path=require('path')
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './public/images/profile_pics');
+
+    },
+    filename:function(req, file, cb){
+        cb(null, `${Date.Now()}_img_${path.extname(file.originalname)}`)
+    }
+})
 const router=express.Router()
+const uploadFile=multer({storage})
 
 
 router.get('/', mainController.index)
