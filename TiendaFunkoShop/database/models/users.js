05f1b7b6240b601b48011edd015a5abec9059278
EveraldoGCH <everaldoguevara@gmail.com>
2021-05-14 22:03:46 -0300

    base de datos creada con MySql

---
+++ TiendaFunkoShop/database/models/users.js
@@ -0,0 +1,32 @@
+module.exports=function(sequelize, dataTypes){
+    let alias="Users";
+    let cols={
+        id_user:{
+        type: dataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+        allowNull:false},
+    
+        user_name:{
+        type:dataTypes.STRING,
+        allowNull:false},
+    
+        password:{
+        type:dataTypes.STRING,
+        allowNull:false},
+    
+        profiel_img:{
+        type:dataTypes.INTEGER,
+        allowNull:false}
+    };
+    
+    let config={
+        table_name:"users",
+        timestamps:false
+    }
+    
+    
+        let Users=sequelize.define(alias, cols, config)
+    
+        return Users
+    }
